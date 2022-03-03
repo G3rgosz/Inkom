3b15e15b170fc6d31352d818330d99c075342032
Gergosz-2000 <gergoszivak@gmail.com>
2022-03-03 11:50:25 +0100

    componensek kesz

---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+  employeeName = "Erős István";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
